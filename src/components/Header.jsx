8da060c904a16cfa101f25e42100198441c7884e
blevinspercussion <blevinspercussion@gmail.com>
2022-07-29 15:28:32 -0500

    Began mobile design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ function Header () {
             <nav class="nav">
             <img class="logo" src={logo} />
                 <ul className='nav-menu'>
+                    <i class="fa-solid fa-bars mobile"></i>
                     <li class="link section-link active-link" id="about-link" href='#about-section'><a>about</a></li>
                     <li class="link section-link" id="projects-link" href='.projects'><a>projects</a></li>
                     <li class="link section-link" id="links-link"><a>links</a></li>
